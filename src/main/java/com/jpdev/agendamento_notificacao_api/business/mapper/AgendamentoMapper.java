f21030f6f7f4dad4fa3bbe02ff4bb01875027609
João Paulo Lima <jplopapa.dev@gmail.com>
2026-02-24 20:47:39 -0300

    Adicionado método que cancela agendamento

--- src/main/java/com/jpdev/agendamento_notificacao_api/business/mapper/AgendamentoMapper.java
+++ src/main/java/com/jpdev/agendamento_notificacao_api/business/mapper/AgendamentoMapper.java
@@ -3,7 +3,11 @@ package com.jpdev.agendamento_notificacao_api.business.mapper;
 import com.jpdev.agendamento_notificacao_api.business.dtos.in.AgendamentoDTOIn;
 import com.jpdev.agendamento_notificacao_api.business.dtos.out.AgendamentoDTOOut;
 import com.jpdev.agendamento_notificacao_api.infrastructure.entities.Agendamento;
+import com.jpdev.agendamento_notificacao_api.infrastructure.enums.StatusNotificacaoEnum;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.time.LocalDateTime;
 
 @Mapper(componentModel = "spring")
 public interface AgendamentoMapper {
@@ -12,5 +16,16 @@ public interface AgendamentoMapper {
 
     AgendamentoDTOOut paraAgendamentoOut(Agendamento agendamento);
 
+    @Mapping(target = "dataHoraModificacao", expression = "java(agora())")
+    @Mapping(target = "statusNotificacao", expression = "java(cancelar())")
+    Agendamento paraCancelarAgendamento(Agendamento agendamento);
+
+    default LocalDateTime agora(){
+        return LocalDateTime.now();
+    }
+
+    default StatusNotificacaoEnum cancelar(){
+        return StatusNotificacaoEnum.CANCELADO;
+    }
 
 }
